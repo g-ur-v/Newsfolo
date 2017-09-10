dab4588a0d84189f658c3b1dd7f5e568252f891e
iamgrawal <gaurav.rawal66@gmail.com>
2017-09-10 13:45:59 +0530

    Preferences Added

---
+++ app/src/main/java/com/gaurav/android/newsfolo/SettingsActivity.java
@@ -0,0 +1,10 @@
+package com.gaurav.android.newsfolo;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by gaurav on 24/8/17.
+ */
+
+public class SettingsActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/gaurav/android/newsfolo/SingletonVolley.java
@@ -0,0 +1,8 @@
+package com.gaurav.android.newsfolo;
+
+/**
+ * Created by gaurav on 20/8/17.
+ */
+
+public class SingletonVolley {
+}
