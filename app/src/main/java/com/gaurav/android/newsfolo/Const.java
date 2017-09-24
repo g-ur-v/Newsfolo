8a0288ec46bbaf61a8c8338f4c6485724b94ba5f
Gaurav Rawal <gaurav.rawal66@gmail.com>
2017-09-25 01:06:24 +0530

    Urls Updated

--- app/src/main/java/com/gaurav/android/newsfolo/Const.java
+++ app/src/main/java/com/gaurav/android/newsfolo/Const.java
@@ -10,4 +10,18 @@ class Const {
     static final String EntertainmentUrl = "http://test.newsfolo.com/wp-json/wp/v2/posts?filter[category_name]=Entertainment";
     static final String PoliticsUrl = "http://test.newsfolo.com/wp-json/wp/v2/posts?filter[category_name]=Politics";
     static final String SportsUrl = "http://test.newsfolo.com/wp-json/wp/v2/posts?filter[category_name]=Sports";
+    
+    static final String HomeUrlXml = "https://www.newsfolo.com/feed/";
+    static final String EducationUrlXml = "https://www.newsfolo.com/category/education/feed";
+    static final String IndiaUrlXml = "https://www.newsfolo.com/category/india/feed";
+    static final String BusinessUrlXml = "https://www.newsfolo.com/category/business/feed";
+    static final String EntertainmentUrlXml = "https://www.newsfolo.com/category/entertainment/feed";
+    static final String SportsUrlXml = "https://www.newsfolo.com/category/sports/feed";
+    static final String TechnologyUrlXml = "https://www.newsfolo.com/category/technology/feed";
+    static final String LifestyleUrlXml = "https://www.newsfolo.com/category/lifestyle/feed";
+    static final String EditorschoiceUrlXml = "https://www.newsfolo.com/category/editors-picks/feed";
+    static final String PhotosUrlXml = "https://www.newsfolo.com/category/photos/feed";
+    static final String PoliticsUrlXml = "https://www.newsfolo.com/category/politics/feed";
+    static final String HeadlinesUrlXml = "https://www.newsfolo.com/category/headlines/feed";
+    static final String WorldUrlXml = "https://www.newsfolo.com/category/world/feed";
 }
